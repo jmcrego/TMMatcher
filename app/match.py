b1a408e3fd0476f1876004915a1e45fd6695b456
Josep Crego <jmcrego@gmail.com>
2026-03-29 11:21:54 +0200

    Implement fuzzy matching in rescore function and update match_endpoint to use it

--- app/match.py
+++ app/match.py
@@ -4,6 +4,8 @@ from pydantic import BaseModel
 from typing import List
 from .shared import indices, indices_lock, tokenize
 
+from rapidfuzz import fuzz
+
 class TMMatchRequest(BaseModel):
     sentence: str
     indices: List[str]
@@ -18,6 +20,18 @@ class TMMatchResponse(BaseModel):
     matches: List[TMMatchResult]
     runtime_ms: float = 0
 
+def rescore(query_tokens, results, scores, k=5):
+    # replace the score by a fuzzy match score for all retrieved documents, 
+    # FMS = 1 - edit_distance(query, source) / max(len(query), len(source)) # normalized edit distance
+    for b in range(results.shape[0]):
+        for i in range(results.shape[1]):
+            doc = results[b, i]
+            source_tokens = tokenize(doc['source'])
+            score = fuzz.edit_distance(query_tokens, source_tokens)
+            scores[b, i] = 1 - score / max(len(query_tokens), len(source_tokens)) # normalized edit distance
+    # Get the indices of the top-k scores over each row (query)
+    top_k_indices = scores.argsort(axis=1)[:, :k] # sort ascending and take top k shape is (n_queries, k)
+    return results[:, top_k_indices], scores[:, top_k_indices]
 
 def match_endpoint(request: TMMatchRequest) -> TMMatchResponse:
     results = []
@@ -29,7 +43,8 @@ def match_endpoint(request: TMMatchRequest) -> TMMatchResponse:
             if not entry:
                 continue
             automaton = entry["automaton"]
-            results, scores = automaton.retrieve([query_tokens], k=2)
+            results, scores = automaton.retrieve([query_tokens], k=10)
+            results, scores = rescore(query_tokens, results, scores, k=5)
             for i in range(results.shape[1]):
                 doc, score = results[0, i], scores[0, i]
                 results.append(TMMatchResult(
